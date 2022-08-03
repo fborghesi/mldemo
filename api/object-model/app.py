8bd591c032c8fca91cfd62dc5277b2a56eeb524c
Franco Borghesi <franco.borghesi@gmail.com>
2022-08-03 16:28:16 -0300

    Completed OAUTH2 integration with google

--- api/object-model/app.py
+++ api/object-model/app.py
@@ -2,21 +2,72 @@ from lib.utils.response import Response
 import tensorflow_hub as hub
 import tensorflow as tf
 from object_detection.utils import visualization_utils as viz_utils
+from object_detection.utils import label_map_util
 from six import BytesIO
 from PIL import Image
 import numpy as np
 import base64
-from object_detection.utils import label_map_util
 import os
+import shutil
+import time
+import tarfile
 import logging
 import lib.common.log_config
 
 BASE_DIR = '/opt/mlmodels/objects'
-HUB_MODEL_DIR = os.path.join(BASE_DIR, 'tfhub_model')
+URL_FILE_NAME = os.path.join(BASE_DIR, 'tfhub_model', 'url.txt')
+HUB_MODEL_SRC_TAR_FILE = os.path.join(BASE_DIR, 'tfhub_model', '1.tar.gz')
+HUB_MODEL_DIR_TGT = '/tmp/tfhub_model'
+HUB_MODEL_DIR_LOCK = HUB_MODEL_DIR_TGT + '.lock'
 LABEL_MAP_PATH = os.path.join(BASE_DIR, 'mscoco_label_map.pbtxt')
 MIME_TYPE = 'image/png'
 MIN_SCORE = 0.5
 logger = logging.getLogger(__name__)
+
+
+def target_exists():
+    return os.path.isdir(HUB_MODEL_DIR_TGT)
+
+
+def lock_exists():
+    return os.path.isdir(HUB_MODEL_DIR_LOCK)
+
+
+def wait_on_lock():
+    count = 0
+    if lock_exists():
+        while not target_exists() and count < 60:
+            time.sleep(1)
+            count += 1
+
+    return target_exists()
+
+
+def extract_model_lock():
+    logger.info(
+        "Extracting tensorflow model from {src} into lock dir {tgt}.".format(src=HUB_MODEL_SRC_TAR_FILE, tgt=HUB_MODEL_DIR_LOCK))
+    if not lock_exists():
+        os.mkdir(HUB_MODEL_DIR_LOCK)
+    tar = tarfile.open(HUB_MODEL_SRC_TAR_FILE, "r:gz")
+    tar.extractall(HUB_MODEL_DIR_LOCK)
+    tar.close()
+
+
+def rename_lock():
+    if not target_exists():
+        logger.info("Moving tensorflow model from lock dir {src} to {tgt}.".format(src=HUB_MODEL_DIR_LOCK,
+                                                                                   tgt=HUB_MODEL_DIR_TGT))
+        shutil.move(HUB_MODEL_DIR_LOCK, HUB_MODEL_DIR_TGT)
+
+
+# model does not exist in /tmp?
+if not target_exists():
+    logger.info("Target directory {tgt} not found.".format(tgt=HUB_MODEL_DIR_TGT))
+
+    # copy model if no copy in progress, or wait for it to finish otherwise
+    if not lock_exists() or not wait_on_lock():
+        extract_model_lock()
+        rename_lock()
 
 # load class labels
 logger.info("Loading labels from {}.".format(LABEL_MAP_PATH))
@@ -43,13 +94,13 @@ COCO17_HUMAN_POSE_KEYPOINTS = [(0, 1),
 
 
 # be sure you run <b>make</b> on current directory to install desired model
-with open(os.path.join(HUB_MODEL_DIR, 'url.txt'), 'r') as file:
+with open(URL_FILE_NAME, 'r') as file:
     url = file.read().rstrip()
 
 # load the model
-logger.info("Loading cached model downloaded from URL {} from {}.".format(url, HUB_MODEL_DIR))
+logger.info("Loading cached model downloaded from URL {} from {}.".format(url, HUB_MODEL_DIR_TGT))
 load_options = tf.saved_model.LoadOptions(experimental_io_device='/job:localhost')
-hub_model = hub.load(HUB_MODEL_DIR, options=load_options)
+hub_model = hub.load(HUB_MODEL_DIR_TGT, options=load_options)
 
 def load_img(self, src):
     data = BytesIO(tf.io.gfile.GFile(src, 'rb').read())
@@ -58,10 +109,11 @@ def load_img(self, src):
     return np.array(img.getdata()).reshape((1, h, w, 3)).astype(np.uint8)
 
 
-def predict(img):
+def predict(img_data):
     label_id_offset = 0
 
     # resize the image
+    img = Image.open(BytesIO(img_data))
     (w, h) = img.size
     img_arr = np.array(img.getdata()).reshape((1, h, w, 3)).astype(np.uint8)
 
@@ -99,12 +151,15 @@ def predict(img):
 
 def lambda_handler(event, context):
     try:
-        body = base64.b64decode(event['body'])
+        body = event['body']
+        body_data = base64.b64decode(body)
 
         # do prediction
-        result = predict(body)
+        prediction = predict(body_data)
 
-        return Response.success(200, result, MIME_TYPE).to_json()
+        # set up response
+        result = base64.b64encode(prediction)
+        return Response.success(200, result, MIME_TYPE, True).to_json()
     except Exception as e:
         logger.exception(e)
 
--- api/user/login/app.py
+++ api/user/login/app.py
@@ -6,9 +6,50 @@ from lib.validators.validation_exception import ValidationException
 from lib.db.user_helper import UserHelper
 from lib.db import mongodb
 import logging
+from oauth2client import client
+from lib.validators import token
+from lib.db.db_helper_exception import DatabaseHelperException
+from lib.aws.secret_mgr_helper import SecretsManagerHelper
+import urllib.parse
+import os
 import lib.common.log_config
 
 logger = logging.getLogger(__name__)
+
+CLIENT_SECRET_JSON = "/var/task/lib/validators/client_secret.json"
+LAMBDA_ENVIRONMENT = os.environ['LAMBDA_ENVIRONMENT']
+GOOGLE_AUTH_REDIRECT_URL = os.environ['GOOGLE_AUTH_REDIRECT_URL']
+GOOGLE_AUTH_SECRET_NAME = '{env}.GoogleAuthSecret'.format(env=LAMBDA_ENVIRONMENT)
+secret_mgr_helper = SecretsManagerHelper()
+google_auth_secret = secret_mgr_helper.get_secret(GOOGLE_AUTH_SECRET_NAME)
+
+SCOPES = [
+    "https://www.googleapis.com/auth/userinfo.email",
+    "https://www.googleapis.com/auth/userinfo.profile"
+];
+
+def auth_google(auth_code):
+    client_config = ujson.load(open(CLIENT_SECRET_JSON, 'r'))['web']
+
+    try:
+        logger.info("Getting credentials for code {auth_code}".format(auth_code=auth_code))
+
+        redirect_uri = client_config["redirect_uris"][0]
+        credentials = client.credentials_from_code(client_config["client_id"], google_auth_secret["client_secret"], SCOPES, auth_code,
+            redirect_uri=redirect_uri)
+        #redirect_uri='postmessage'
+
+        logger.info(
+            "User information for code {code}: {user_info}".format(code=auth_code, user_info=str(credentials.id_token)))
+
+        # get field data from credentials
+        return credentials.id_token
+
+    except client.FlowExchangeError as e:
+        logger.exception(e)
+        raise ValidationException(str(e))
+
+
 
 def user_to_dto(d):
     return {k: v for k, v in d.items() if k != 'password' }
@@ -18,19 +59,48 @@ def lambda_handler(event, context):
     user_helper = UserHelper(mongodb)
 
     try:
-        body = ujson.loads(event["body"])
-        errors = validator.validate(body)
+        if event["httpMethod"] == "POST":
+            body = ujson.loads(event["body"])
+            errors = validator.validate(body)
 
-        if errors:
-            raise ValidationException.from_error_dict(errors)
+            if errors:
+                raise ValidationException.from_error_dict(errors)
 
-        data = validator.load(body)
-        logger.info("Schema: {}".format(str(data)))
-        email = data['email']
-        token = data['token']
+            data = validator.load(body)
+            logger.info("Schema: {}".format(str(data)))
 
-        result = user_helper.get_by_email(email)
-        result["token"] = token
+            result = user_helper.get_by_email(data['email'])
+            result["token"] = data['token']
+        else:
+            query_string = event["queryStringParameters"]
+            data = auth_google(query_string['code'])
+
+            try:
+                # find and update user in db
+                found = user_helper.get_by_email(data['email'])
+                logger.info("User found in database: {found}".format(found=found))
+
+                if not found["is_active"]:
+                    raise ValidationException("Your account is pending approval from Administrators.")
+
+                found["first_name"] = data["given_name"]
+                found["last_name"] = data["family_name"]
+                found["email"] = data["email"]
+                logger.info("Updating user with data: ", found)
+
+                user_helper.update(found["id"], found)
+
+                result = found
+                result["token"] = token.create_access_token(found)
+                uri = "{redirect_url}?{params}".format(redirect_url=GOOGLE_AUTH_REDIRECT_URL, params=urllib.parse.urlencode(result))
+                return Response.redirect(uri).to_json()
+
+            except DatabaseHelperException as e:
+                # track user for the future
+                dto = {'first_name': data["given_name"], 'last_name': data["family_name"], 'email': data["email"], 'is_admin': False,
+                       'is_active': False}
+                user_helper.create(dto)
+                raise ValidationException(str(e))
 
         return Response.success(200, result).to_json()
     except ValidationException as e:
