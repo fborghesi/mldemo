179c6c8b40813f14d7692b85c8d853a86b4035fc
fborghesi <54476705+fborghesi@users.noreply.github.com>
2023-08-09 11:32:53 -0300

    Added speech recognition using whisper-1

--- api/s2t-model/app.py
+++ api/s2t-model/app.py
@@ -1,30 +1,34 @@
-from lib.utils.response import Response
-from keras.preprocessing.image import img_to_array
-from keras.models import model_from_json
-import numpy as np
 import os
-from PIL import Image
-import base64
 from io import BytesIO
+
+from lib.aws.secret_mgr_helper import SecretsManagerHelper
+from lib.utils.response import Response
+import base64
 import logging
 import lib.common.log_config
 import openai
 
+
 logger = logging.getLogger(__name__)
 
 # get the open ai api key
+LAMBDA_ENVIRONMENT = os.environ['LAMBDA_ENVIRONMENT']
 OPENAI_API_KEY = '{env}.OpenAIApiKey'.format(env=LAMBDA_ENVIRONMENT)
 secrets_helper = SecretsManagerHelper()
 secret = secrets_helper.get_secret(OPENAI_API_KEY)
-assert secret is not None, 'Could not retrieve OpenAI API Key "{secret}!"'.format(secret=OPENAI_API_KEY)
 
-openai.api_key = secret['key']
+assert secret is not None, 'Could not retrieve OpenAI API Key "{secret}!"'.format(secret=OPENAI_API_KEY)
+openai.api_key = secret['api_key']
 
 def lambda_handler(event, context):
     try:
         body = event['body']
         audio = base64.b64decode(body)
-        result = openai.Audio.transcribe("Whisper-1", body)
+
+        buffer = BytesIO(audio)
+        buffer.name = "input.mp3"
+
+        result = openai.Audio.transcribe("whisper-1", buffer)
         return Response.success(200, result).to_json()
 
     except Exception as e:
